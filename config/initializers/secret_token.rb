abb70209ac6f650362b7f0bb1a73f4150207c3fc
Sidu Ponnappa <ckponnappa@gmail.com>
2014-07-29 11:39:04 +0530

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StartingPoint::Application.config.secret_key_base = '638a27a19839f16d071d4929adc313bf3e39459951f2642c31602ff53479eaa3a20cff338ceae135f1776d081058abe41e00b62f2426de5216e7e90e5e1c28e2'
---
+++ spec/models/delete_me_spec.rb
@@ -0,0 +1,5 @@
+require "rails_helper"
+
+describe "Delete" do
+  it "this file please"
+end
